30655cf7ef155635df11098e5e3012b4041de291
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-07-12 22:36:33 +0300

    Add month and year validation

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var form = document.querySelector('.identification-head_bank form');
 var cvc = form.querySelector('.card-cvc');
 var user = form.querySelector('.card-user');
 var cardNumber = form.querySelectorAll('.card-number');
+var month = form.querySelector('.card-month');
+var year = form.querySelector('.card-year');
 
 validateForm = () => {
   event.preventDefault();
@@ -9,7 +11,7 @@ validateForm = () => {
   removeValidation();
   checkErrors();
 
-  var errors = document.querySelectorAll('.error');
+  const errors = document.querySelectorAll('.error');
 
   if (!errors.length) {
     return true;
@@ -19,7 +21,7 @@ validateForm = () => {
 };
 
 removeValidation = () => {
-  var errors = document.querySelectorAll('.error');
+  const errors = document.querySelectorAll('.error');
 
   for (let i = 0; i < errors.length; i++) {
     errors[i].classList.remove('error');
@@ -27,6 +29,9 @@ removeValidation = () => {
 };
 
 checkErrors = () => {
+  const dYear = new Date().getFullYear();
+  const dMonth = new Date().getMonth();
+
   for (let i = 0; i < cardNumber.length; i++) {
     if (cardNumber[i].value.length !== 4)
       cardNumber[i].className += ' ' + 'error';
@@ -34,4 +39,6 @@ checkErrors = () => {
   if (cvc.value.length !== 3) cvc.className += ' ' + 'error';
   if (!/^([- A-Za-z0-9]+){4,}$/.test(user.value))
     user.className += ' ' + 'error';
+  if (Number(month.value) < dMonth && Number(year.value) === dYear)
+    month.className += ' ' + 'error';
 };
